c19ae069fda461ffacd1834644224b5786b516b0
saintzema <87872613+saintzema@users.noreply.github.com>
2023-05-29 16:11:31 +0100

    translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -26,7 +26,7 @@ def english_to_french(english_text):
 
     print(json.dumps(frenchtranslation, indent=2, ensure_ascii=False))
 
-    return frenchtranslation.get("translations")[0].get("translation")
+    return frenchtranslation.get("translations")[0].get("frenchtranslation")
 
 """Function to translate French to English"""
 def french_to_english(french_text):
@@ -44,4 +44,4 @@ def french_to_english(french_text):
 
     print(json.dumps(englishtranslation, indent=2, ensure_ascii=False))
 
-    return englishtranslation.get("translations")[0].get("translation")
+    return englishtranslation.get("translations")[0].get("englishtranslation")
